ed473150302c349e9f22635a8ad855b1597d19e3
Adam Feldman <adamlee.feldman@post.runi.ac.il>
2025-11-24 20:39:48 +0200

    Added API request logic with params, succesfully fetches data from econstor API

--- main.py
+++ main.py
@@ -1,5 +1,30 @@
+import requests
+import json
+
+BASE_URL = "https://api.econbiz.de/v1/search"
+DEFAULT_SIZE = 10
+
+
 def main():
-    print("Hello from econstor-scraper!")
+    query = input("Enter search term: ")
+    print(f"Searching for: {query}")
+  
+
+    params = { 
+        "q": query,
+        "ff": 'source:"econstor"', 
+        "from": 1,
+        "size": DEFAULT_SIZE }
+
+    print("Making API request")
+    response = requests.get(BASE_URL, params=params)
+
+    if response.status_code == 200:
+        data = response.json()
+        print("success, here's requested data")
+        print(json.dumps(data))
+
+    else: print(f"Error: {response.status_code}")
 
 
 if __name__ == "__main__":
